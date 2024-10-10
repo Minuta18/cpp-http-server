5355906ea9f36926ceb4321cba4bb50defaaaf0f
Minuta18 <ig.samsonov10@yandex.ru>
2024-10-10 11:46:32 +0300

    Application class

---
+++ src/Application/Application.h
@@ -0,0 +1,11 @@
+#ifndef APPLICATION_APPLICATION_
+#define APPLICATION_APPLICATION_
+
+namespace http_server {
+    class Application {
+    public:
+        Application() {}
+    };
+}
+
+#endif // APPLICATION_APPLICATION_
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include "./Application/Application.h"
 
 int main() {
+    http_server::Application app;
+
     std::cout << "Hello cmake!" << std::endl;
 }
